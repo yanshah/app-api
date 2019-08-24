4ca0418d5eefe732f09265bb9d9d1689ea8d590d
Yan Shah <yan.shah@hotmail.fr>
2019-08-24 15:00:23 +0200

    User management is added with superuser creation

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email(self):
+        """ Test creating a new user with email """
+        email = 'test@yan.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the @email part is lowercased"""
+        email = 'yan@YAN.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Tests user invalid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+
+
+    def test_create_superuser(self):
+        """Tests superuser creation"""
+        email = 'test@yan.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_supperuser)
+        self.assertTrue(user.is_staff)
